101aa72bb4ff85083fb544bf2737b12bf09b1158
Vladislav_Zembickiy <v777888999v@gmail.com>
2015-12-06 11:53:54 +0200

    dialog video

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,13 @@
-angular.module('cwist', ["ngMaterial"])
-.controller('AppCtrl', function($scope) {});
-
+angular.module('cwist', ["ngMaterial", "ngDialog"])
+.controller('openVideo', function ($scope, $log, ngDialog) {});
 
 $("#lessonsSlider").owlCarousel({
     items : 2,
+    itemsDesktop : [1199,2],
+    itemsDesktopSmall : [980,2],
+    itemsTablet: [768,2],
+    itemsMobile : [600,1],
+
     navigation : true,
     pagination : false, 
     navigationText : ["",""],    
