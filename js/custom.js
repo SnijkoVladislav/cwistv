295d4a2d1984941969d245a23112c5a1436097ce
Vladislav_Zembickiy <v777888999v@gmail.com>
2015-12-08 18:40:53 +0200

    fix slider

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,37 @@
 $.material.init();
 $.material.ripples();
 
-$("#lessonsSlider").owlCarousel({
-    items : 2,
-    itemsDesktop : [1199,2],
-    itemsDesktopSmall : [980,2],
-    itemsTablet: [768,2],
-    itemsMobile : [600,1],
+$(function () {
+	$(".youtube").YouTubeModal({autoplay:0});
+});
 
-    navigation : true,
-    pagination : false, 
-    navigationText : ["",""],    
+// $("#lessonsSlider").owlCarousel({
+//     items : 2,
+//     itemsDesktop : [1199,2],
+//     itemsDesktopSmall : [980,2],
+//     itemsTablet: [768,2],
+//     itemsMobile : [600,1],
+
+//     navigation : true,
+//     pagination : false, 
+//     navigationText : ["",""],    
+// });
+
+$('#theCarousel').carousel({
+  interval: false
+})
+
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  // next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  }
+  else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
 });
